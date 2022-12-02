7a9eb91562f46f4d301301691587c0f8418823fa
mamonu <thorosm@msn.com>
2022-12-02 10:21:28 +0000

    day1

--- Day1.scala
+++ Day1.scala
@@ -1,11 +1,18 @@
 import scala.io.Source
 
+object day1
+{
+
+def main(args:Array[String]):Unit ={
+
 val inputFile = Source.fromFile("inputs/01-input.txt")
 val cals = inputFile.getLines().map(_.split("\n")).map(_.map(_.toInt))
 val totalCals = cals.map(_.sum)
 
 // Q1
-totalCals.max
+println(totalCals.max)
 
 // Q2
-totalCals.sorted(Ordering[Int].reverse).take(3).sum
+println(totalCals.sorted(Ordering[Int].reverse).take(3).sum)
+    }
+}
